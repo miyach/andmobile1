fa8f1603884dbe89f99c2e88fe4fb90cd46f59cf
mychen76 <mychen76@gmail.com>
2013-04-12 02:34:10 -0400

    updated

--- Simple-Android-OCR/src/com/mc76/ocr/prototype/OCRPrototypeActivity.java
+++ Simple-Android-OCR/src/com/mc76/ocr/prototype/OCRPrototypeActivity.java
@@ -1,15 +1,17 @@
-package com.datumdroid.android.ocr.simple;
+package com.mc76.ocr.prototype;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.zip.GZIPInputStream;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -23,23 +25,25 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
-public class SimpleAndroidOCRActivity extends Activity {
-	public static final String PACKAGE_NAME = "com.datumdroid.android.ocr.simple";
+public class OCRPrototypeActivity extends Activity {
+	public static final String PACKAGE_NAME = "com.mc76.ocr.prototype";
 	public static final String DATA_PATH = Environment
-			.getExternalStorageDirectory().toString() + "/SimpleAndroidOCR/";
-	
+			.getExternalStorageDirectory().toString() + "/OCRPrototype/";
+
 	// You should have the trained data file in assets folder
 	// You can get them at:
 	// http://code.google.com/p/tesseract-ocr/downloads/list
 	public static final String lang = "eng";
 
-	private static final String TAG = "SimpleAndroidOCR.java";
+	private static final String TAG = "OCRPrototypeActivity.java";
 
 	protected Button _button;
-	// protected ImageView _image;
+	protected ImageView _image;
 	protected EditText _field;
 	protected String _path;
 	protected boolean _taken;
@@ -55,7 +59,8 @@ public class SimpleAndroidOCRActivity extends Activity {
 			File dir = new File(path);
 			if (!dir.exists()) {
 				if (!dir.mkdirs()) {
-					Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+					Log.v(TAG, "ERROR: Creation of directory " + path
+							+ " on sdcard failed");
 					return;
 				} else {
 					Log.v(TAG, "Created directory " + path + " on sdcard");
@@ -63,34 +68,41 @@ public class SimpleAndroidOCRActivity extends Activity {
 			}
 
 		}
-		
+
 		// lang.traineddata file with the app (in assets folder)
 		// You can get them at:
 		// http://code.google.com/p/tesseract-ocr/downloads/list
 		// This area needs work and optimization
-		if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+		if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata"))
+				.exists()) {
 			try {
 
 				AssetManager assetManager = getAssets();
 				InputStream in = assetManager.open("tessdata/eng.traineddata");
-				//GZIPInputStream gin = new GZIPInputStream(in);
 				OutputStream out = new FileOutputStream(DATA_PATH
 						+ "tessdata/eng.traineddata");
 
 				// Transfer bytes from in to out
 				byte[] buf = new byte[1024];
 				int len;
-				//while ((lenf = gin.read(buff)) > 0) {
 				while ((len = in.read(buf)) > 0) {
 					out.write(buf, 0, len);
 				}
 				in.close();
-				//gin.close();
 				out.close();
-				
+
 				Log.v(TAG, "Copied " + lang + " traineddata");
+				Toast.makeText(getBaseContext(),
+						"Copied " + lang + " traineddata", Toast.LENGTH_SHORT)
+						.show();
 			} catch (IOException e) {
-				Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+				Log.e(TAG,
+						"Was unable to copy " + lang + " traineddata "
+								+ e.toString());
+				Toast.makeText(
+						getBaseContext(),
+						"Was unable to copy " + lang + " traineddata "
+								+ e.toString(), Toast.LENGTH_SHORT).show();
 			}
 		}
 
@@ -98,7 +110,7 @@ public class SimpleAndroidOCRActivity extends Activity {
 
 		setContentView(R.layout.main);
 
-		// _image = (ImageView) findViewById(R.id.image);
+		_image = (ImageView) findViewById(R.id.image);
 		_field = (EditText) findViewById(R.id.field);
 		_button = (Button) findViewById(R.id.button);
 		_button.setOnClickListener(new ButtonClickHandler());
@@ -109,6 +121,10 @@ public class SimpleAndroidOCRActivity extends Activity {
 	public class ButtonClickHandler implements View.OnClickListener {
 		public void onClick(View view) {
 			Log.v(TAG, "Starting Camera app");
+			_field.setText("");
+
+			Toast.makeText(getBaseContext(), "Starting Camera app",
+					Toast.LENGTH_SHORT).show();
 			startCameraActivity();
 		}
 	}
@@ -120,10 +136,17 @@ public class SimpleAndroidOCRActivity extends Activity {
 		File file = new File(_path);
 		Uri outputFileUri = Uri.fromFile(file);
 
-		final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-
-		startActivityForResult(intent, 0);
+		if (isIntentAvailable(this.getBaseContext(),
+				MediaStore.ACTION_IMAGE_CAPTURE)) {
+			final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+			startActivityForResult(intent, 0);
+			Log.v(TAG, "found camera, start capturing image");
+		} else {
+			Log.v(TAG, "camera not found!");
+			Toast.makeText(getBaseContext(), "Camera not found in the phone!",
+					Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	@Override
@@ -132,21 +155,30 @@ public class SimpleAndroidOCRActivity extends Activity {
 		Log.i(TAG, "resultCode: " + resultCode);
 
 		if (resultCode == -1) {
+			Log.v(TAG, "preview photo");
+			// Bundle extras = data.getExtras();
+			// Bitmap mImageBitmap = (Bitmap) extras.get("data");
+			// _image.setImageBitmap(mImageBitmap);
+			// process photo
+			Toast.makeText(getBaseContext(), "preview photo",
+					Toast.LENGTH_SHORT).show();
 			onPhotoTaken();
 		} else {
 			Log.v(TAG, "User cancelled");
+			Toast.makeText(getBaseContext(), "User cancelled",
+					Toast.LENGTH_SHORT).show();
 		}
 	}
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
-		outState.putBoolean(SimpleAndroidOCRActivity.PHOTO_TAKEN, _taken);
+		outState.putBoolean(OCRPrototypeActivity.PHOTO_TAKEN, _taken);
 	}
 
 	@Override
 	protected void onRestoreInstanceState(Bundle savedInstanceState) {
 		Log.i(TAG, "onRestoreInstanceState()");
-		if (savedInstanceState.getBoolean(SimpleAndroidOCRActivity.PHOTO_TAKEN)) {
+		if (savedInstanceState.getBoolean(OCRPrototypeActivity.PHOTO_TAKEN)) {
 			onPhotoTaken();
 		}
 	}
@@ -158,7 +190,8 @@ public class SimpleAndroidOCRActivity extends Activity {
 		options.inSampleSize = 4;
 
 		Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
-
+		Toast.makeText(getBaseContext(), "Decode file", Toast.LENGTH_SHORT)
+				.show();
 		try {
 			ExifInterface exif = new ExifInterface(_path);
 			int exifOrientation = exif.getAttributeInt(
@@ -197,6 +230,8 @@ public class SimpleAndroidOCRActivity extends Activity {
 				bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
 			}
 
+			Toast.makeText(getBaseContext(), " Convert to ARGB_8888",
+					Toast.LENGTH_SHORT).show();
 			// Convert to ARGB_8888, required by tess
 			bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
 
@@ -204,39 +239,59 @@ public class SimpleAndroidOCRActivity extends Activity {
 			Log.e(TAG, "Couldn't correct orientation: " + e.toString());
 		}
 
-		// _image.setImageBitmap( bitmap );
-		
+		// show the image
+		_image.setImageBitmap(bitmap);
+
 		Log.v(TAG, "Before baseApi");
+		Toast.makeText(getBaseContext(), "Before baseApi", Toast.LENGTH_SHORT)
+				.show();
 
-		TessBaseAPI baseApi = new TessBaseAPI();
-		baseApi.setDebug(true);
-		baseApi.init(DATA_PATH, lang);
-		baseApi.setImage(bitmap);
-		
-		String recognizedText = baseApi.getUTF8Text();
-		
-		baseApi.end();
+		try {
+			TessBaseAPI baseApi = new TessBaseAPI();
+			baseApi.setDebug(true);
+			baseApi.init(DATA_PATH, lang);
+			baseApi.setImage(bitmap);
+			String recognizedText = baseApi.getUTF8Text();
 
-		// You now have the text in recognizedText var, you can do anything with it.
-		// We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
-		// so that garbage doesn't make it to the display.
+			baseApi.end();
 
-		Log.v(TAG, "OCRED TEXT: " + recognizedText);
+			// You now have the text in recognizedText var, you can do anything
+			// with it.
+			// We will display a stripped out trimmed alpha-numeric version of
+			// it (if lang is eng)
+			// so that garbage doesn't make it to the display.
 
-		if ( lang.equalsIgnoreCase("eng") ) {
-			recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
-		}
-		
-		recognizedText = recognizedText.trim();
+			Log.v(TAG, "OCRED TEXT: " + recognizedText);
+			Toast.makeText(getBaseContext(), "OCRED TEXT: " + recognizedText,
+					Toast.LENGTH_SHORT).show();
 
-		if ( recognizedText.length() != 0 ) {
-			_field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
-			_field.setSelection(_field.getText().toString().length());
-		}
-		
+			if (lang.equalsIgnoreCase("eng")) {
+				recognizedText = recognizedText
+						.replaceAll("[^a-zA-Z0-9]+", " ");
+			}
+			recognizedText = recognizedText.trim();
+
+			if (recognizedText.length() != 0) {
+				_field.setText(_field.getText().toString().length() == 0 ? recognizedText
+						: _field.getText() + " " + recognizedText);
+				_field.setSelection(_field.getText().toString().length());
+				//display boxes
+				// _image.setImageBitmap(bitmap);
+			}
+		} catch (Exception e) {
+			// e.printStackTrace();
+			Toast.makeText(getBaseContext(), "error: " + e.getMessage(),
+					Toast.LENGTH_SHORT).show();
+		} 
 		// Cycle done.
 	}
-	
-	// www.Gaut.am was here
-	// Thanks for reading!
+
+	public static boolean isIntentAvailable(Context context, String action) {
+		final PackageManager packageManager = context.getPackageManager();
+		final Intent intent = new Intent(action);
+		List<ResolveInfo> list = packageManager.queryIntentActivities(intent,
+				PackageManager.MATCH_DEFAULT_ONLY);
+		return list.size() > 0;
+	}
+
 }
